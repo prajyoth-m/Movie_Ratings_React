2ef6882bedfd0e178dacc03863245bf86e8b02ce
prajyoth-m <m.prajyoth@live.com>
2022-03-07 13:44:55 +0530

    add user feature

--- movie_ratings/src/pages/Users/Users.tsx
+++ movie_ratings/src/pages/Users/Users.tsx
@@ -12,7 +12,29 @@ import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import DirectionsIcon from "@mui/icons-material/Directions";
 import axios from "axios";
-import { CardContent, Stack } from "@mui/material";
+import {
+  CardContent,
+  FormControl,
+  MenuItem,
+  Stack,
+  TextField,
+} from "@mui/material";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import { TransitionProps } from "@mui/material/transitions";
+import Button from "@mui/material/Button";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import InputAdornment from "@mui/material/InputAdornment";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 class User {
   username: string;
@@ -20,7 +42,7 @@ class User {
   lastname: string;
   password: string;
   email: string;
-  phone: BigInteger;
+  phone: number;
   department: string;
 
   constructor(
@@ -29,7 +51,7 @@ class User {
     lastname: string,
     password: string,
     email: string,
-    phone: BigInteger,
+    phone: number,
     department: string
   ) {
     this.username = username;
@@ -42,17 +64,84 @@ class User {
   }
 }
 
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 export default function UsersCard() {
   const [state, setState] = React.useState({
     cardElevation: 2,
+    username: "",
+    firstname: "",
+    lastname: "",
+    showPassword: false,
+    password: "",
+    email: "",
+    department: "",
+    phone: 0,
     users: Array<User>(),
+    departments: Array<string>(),
   });
 
-  const handleDeleteClick = (username: string) => {
-    console.log("delete this user " + username);
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
   };
 
-  React.useEffect(() => {
+  const handleClickShowPassword = () => {
+    setState({
+      ...state,
+      showPassword: !state.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event: any) => {
+    event.preventDefault();
+  };
+
+  const handleClose = (method: string) => {
+    if (method === "save") {
+      let user = new User(
+        state.username,
+        state.firstname,
+        state.lastname,
+        state.password,
+        state.email,
+        state.phone,
+        state.department
+      );
+      axios.post("/user-services/user", user).then(res=>{
+        if (res.data.success) {
+          refresh();
+        }
+      });
+    }
+    setOpen(false);
+  };
+
+  const handleDeleteClick = (username: string) => {
+    axios.delete("/user-services/user/" + username).then((res) => {
+      if (res.data.success) {
+        refresh();
+      }
+    });
+  };
+
+  const handleChange = (prop: any) => (event: any) => {
+    setState({ ...state, [prop]: event.target.value });
+  };
+
+  const handleDeptChange = (event: SelectChangeEvent) => {
+    setState({ ...state, department: event.target.value as string });
+  };
+
+  const refresh = () => {
     axios.get("/user-services/users").then((res) => {
       let data = res.data;
       setState((s) => ({
@@ -64,7 +153,7 @@ export default function UsersCard() {
             lastname: string;
             password: string;
             email: string;
-            phone: Uint8Array;
+            phone: number;
             department: string;
           }) => {
             return new User(
@@ -78,6 +167,18 @@ export default function UsersCard() {
             );
           }
         ),
+      }));
+    });
+  };
+
+  React.useEffect(() => {
+    refresh();
+    axios.get("/department-services/department").then((res) => {
+      setState((s) => ({
+        ...s,
+        departments: res.data.map((dept: { department: any }) => {
+          return dept.department;
+        }),
       }));
     });
   }, []);
@@ -172,6 +273,113 @@ export default function UsersCard() {
           </Card>
         ))}
       </Stack>
+      <Fab
+        color="primary"
+        aria-label="add"
+        sx={{ position: "absolute", bottom: "3.5rem", right: "3.5rem" }}
+        onClick={handleClickOpen}
+      >
+        <AddIcon />
+      </Fab>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"Want to add a new user?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Please provide the below details to add a user.
+          </DialogContentText>
+          <TextField
+            id="outlined-basic"
+            label="Username"
+            fullWidth
+            sx={{ marginTop: "2rem" }}
+            value={state.username}
+            onChange={handleChange("username")}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Firstname"
+            fullWidth
+            sx={{ marginTop: "2rem" }}
+            value={state.firstname}
+            onChange={handleChange("firstname")}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Lastname"
+            fullWidth
+            sx={{ marginTop: "2rem" }}
+            value={state.lastname}
+            onChange={handleChange("lastname")}
+          />
+          <TextField
+            id="outlined-basic"
+            label="E-Mail ID"
+            fullWidth
+            sx={{ marginTop: "2rem" }}
+            value={state.email}
+            onChange={handleChange("email")}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Phone Number"
+            inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+            fullWidth
+            sx={{ marginTop: "2rem" }}
+            value={state.phone}
+            onChange={handleChange("phone")}
+          />
+          <FormControl sx={{ marginTop: "2rem" }} variant="outlined" fullWidth>
+            <InputLabel htmlFor="outlined-adornment-password">
+              Password
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={state.showPassword ? "text" : "password"}
+              value={state.password}
+              onChange={handleChange("password")}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {state.showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+          <FormControl fullWidth sx={{ marginTop: "2rem" }}>
+            <InputLabel id="demo-simple-select-label">Title</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={state.department}
+              label="Title"
+              onChange={handleDeptChange}
+            >
+              {state.departments.map((el, idx) => (
+                <MenuItem value={el} key={idx}>
+                  {el}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => handleClose("cancel")}>Cancel</Button>
+          <Button onClick={() => handleClose("save")}>Save</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
